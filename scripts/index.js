95181f24cc27806ff405a3f388b64c7f27c227fe
trimikpro <vupotiroy112@gmail.com>
2025-07-30 12:37:00 +0600

    video api load and video display ui

--- scripts/index.js
+++ scripts/index.js
@@ -27,13 +27,74 @@ const displayCategories = (categories) => {
   });
 };
 
-
 //create video load content
-const loadVideos = () =>{
-    fetch('')
+const loadVideos = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => videoDisplay(data.videos))
+    .catch((error) => console.log(error));
+};
+
+/*
+{
+    "category_id": "1001",
+    "video_id": "aaaa",
+    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+    "title": "Shape of You",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+            "profile_name": "Olivia Mitchell",
+            "verified": ""
+        }
+    ],
+    "others": {
+        "views": "100K",
+        "posted_date": "16278"
+    },
+    "description": "Dive into the rhythm of 'Shape of You,
 }
 
 
-// create display load content 
+*/
+
+// create display load content
+const videoDisplay = (videos) => {
+  const videoContainer = document.getElementById("video-container");
+  videos.forEach((video) => {
+    console.log(video);
+    const card = document.createElement("div");
+    card.classList = " card bg-base-100 ";
+    card.innerHTML = `
+     
+          <figure class = "h-[200px]">
+            <img
+              src=${video.thumbnail}
+              alt="Shoes"
+              class = " h-full w-full object-cover rounded-lg"
+              />
+          </figure>
+          <div class="px-0 py-2 flex gap-2">
+              <div>
+                  <img class =" w-10 h-10 rounded-full object-cover" src = ${video.authors[0].profile_picture}/>
+              </div>
+              <div>
+                  <h2 class=" text-[16px] font-bold ">${video.title}</h2>
+                 <div class = " flex  gap-2">
+                    <p class = " text-[12px] text-gray-400"> ${video.authors[0].profile_name}</p>
+                    <img class =" w-4 h-4 " src = "https://img.icons8.com/?size=100&id=59733&format=png&color=228BE6"/>
+                 </div> 
+                  <p class = " text-[12px] text-gray-400"> ${video.others.views}</p>
+
+              </div>
+          
+          </div>
+
+        `;
+
+    videoContainer.append(card);
+  });
+};
 
 loadCategories();
+loadVideos();
